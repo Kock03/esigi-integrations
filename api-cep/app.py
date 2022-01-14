69be6fac3ed5049e8bc07b04657e90df2498b1c6
davikrig-ha <daviluia2@gmail.com>
2022-01-14 09:12:16 -0300

    enable cors

--- api-cep/app.py
+++ api-cep/app.py
@@ -5,14 +5,22 @@ from requests import put, get, post, delete
 from flask import request
 import requests
 from requests.api import request
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask('app')
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/street/<zipCode>')
+@cross_origin()
 def getStreet(zipCode):
 
-    request = requests.get('https://viacep.com.br/ws/' + str(zipCode) + '/json')
+    request = requests.get(
+        'https://viacep.com.br/ws/' + str(zipCode) + '/json')
 
     return request.json()
-        
- 
+
+if __name__ == "__main__":
+
+  app.run(port=8000) 
